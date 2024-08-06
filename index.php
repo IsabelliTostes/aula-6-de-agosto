10895d6588361af17f667600f1a3d365f2a9e52e
IsabelliTostes <isa_tostes@hotmail.com>
2024-08-06 17:23:32 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,94 @@
+<?php
+/*
+    1
+    $usuarios= ["nome" => "Cleitu",
+            "email" => "cleitindugrau@gmail.com",
+            "idade" => "12"];
+
+
+    $teste= is_array ($usuarios);
+    echo $teste;
+
+
+
+    2
+    $usuarios= ["nome" => "Cleitu",
+            "email" => "cleitindugrau@gmail.com",
+            "idade" => "12"];
+            
+            $variavel= 1;
+
+    $teste= is_array ($variavel);
+    echo $teste;
+    
+
+    3
+    $usuarios= ["nome" => "Cleitu",
+            "email" => "cleitindugrau@gmail.com",
+            "idade" => "12"];
+            
+    $teste= array_keys ($usuarios);
+    print_r ($teste);
+    
+    4
+    $lista_de_frutas=['maçã','uva', 'ameixa'];
+
+    $num_elementos= count($lista_de_frutas);
+    echo $num_elementos;
+    
+
+    5
+    $pao= "crocante,macio,recheado";
+    $array= explode (",", $pao, -1);
+    print_r ($array);
+
+
+    6
+    $frase= [' o', ' pão',  ' não',  ' está',  ' recheado'];
+    $resultado= implode ("", $frase);
+    echo $resultado
+
+    7
+    $num=12;
+    while ($num<12) {
+    $num++;
+    }
+    echo 'Fim do Loop';
+
+
+    8
+    $num=1;
+    while ($num<12) {
+    $num++;
+    }
+    echo 'Fim do Loop';
+
+    9
+    $elantra=8000;
+    do {echo 'Entrou no do while';
+    } while ($elantra<800);
+
+    while ($elantra<800) {
+    echo'entrou no while';
+    };
+
+
+    10
+    for ($x= 1; $x<=10; $x++) {
+    echo "$x \n";
+    }
+
+    11
+    $itens= array ('sofá', 'mesa', 'mesa', 'geladeira', 'fogão');
+    print_r ($itens);
+
+    foreach($itens as $item) {
+        echo $item;
+        if ($item=='fogão') { 
+            echo 'Compre um fogão e ganhe uma panela de pressão clock';
+        }
+    }
+    */
+    
+
+?>
